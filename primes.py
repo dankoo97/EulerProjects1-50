5c54718a2d76fff01393f24485f2965849ed7c8a
dankoo97 <66748200+dankoo97@users.noreply.github.com>
2020-08-06 09:39:25 -0400

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,15 +1,15 @@
 # Uses Sieve of Eratosthenes to find primes
 def find_primes_less_than(x):
     p = [True] * (x >> 1)  # Creates a boolean list of odd numbers
-    primes = [2]  # Create the final list of primes, 2 is added since only odd numbers are checked
 
     i = 0
     while i < (int(x ** 0.5) >> 1):  # Only checking up until sqrt of x
         if p[i]:
-            primes.append((i << 1) + 3)
-            p = [prime if ((j << 1) + 3) % primes[-1] != 0 else False for j, prime in enumerate(p)]  # removes multiples of the latest prime from list
+            for j in range((i + 1) * ((i << 1) + 3) + i, len(p), (i << 1) + 3):  # starts at the index of the square of the prime
+                p[j] = False  # removes multiples of the latest prime from list
         i += 1
 
+    primes = [2]  # Create the final list of primes, 2 is added since only odd numbers are checked
     for i, prime in enumerate(p):  # Adds all True values from p and converts them into numbers
         if prime:
             primes.append((i << 1) + 3)
