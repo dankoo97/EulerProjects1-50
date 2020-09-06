3347e8f10d50a5c844ba4705f5a93df7a6905367
dankoo97 <66748200+dankoo97@users.noreply.github.com>
2020-09-06 11:58:05 -0400

    Update pandigital.py

--- pandigital.py
+++ pandigital.py
@@ -4,3 +4,7 @@ def is_n_pandigital(x, n):
 
 def is_pandigital(x):
     return all(str(x).count(str(digit)) == 1 for digit in range(1, 10)) and '0' not in str(x)
+
+
+def is_0_to_n_pandigital(x, n=9):
+    return all(str(x).count(str(digit)) == 1 for digit in range(0, n + 1))
